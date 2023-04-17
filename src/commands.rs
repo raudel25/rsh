53d97d7ed383c277a0462a6adfca802f8cd9330a
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-04-16 23:23:19 -0400

    pipes

--- src/commands.rs
+++ src/commands.rs
@@ -1,9 +1,9 @@
 use std::env;
-use std::path::{Path};
-use std::process::Command;
+use std::path::Path;
+use std::process::{Command, Stdio};
 
 pub trait Execute {
-    fn execute(&self);
+    fn execute(&self, stdin: Stdio, out: bool) -> Stdio;
 }
 
 pub struct CommandSystem<'a> {
@@ -18,8 +18,18 @@ impl CommandSystem<'_> {
 }
 
 impl Execute for CommandSystem<'_> {
-    fn execute(&self) {
-        let command = Command::new(&self.program).args(self.args).spawn();
+    fn execute(&self, stdin: Stdio, out: bool) -> Stdio {
+        let stdout = if out {
+            Stdio::inherit()
+        } else {
+            Stdio::piped()
+        };
+
+        let command = Command::new(&self.program)
+            .args(self.args)
+            .stdin(stdin)
+            .stdout(stdout)
+            .spawn();
 
         match command {
             Ok(mut command) => {
@@ -27,9 +37,16 @@ impl Execute for CommandSystem<'_> {
                     Ok(_) => {}
                     Err(e) => eprintln!("{}", e),
                 };
+
+                if !out {
+                    return Stdio::from(command.stdout.unwrap());
+                }
+                return Stdio::inherit();
             }
             Err(e) => eprintln!("{}", e),
-        }
+        };
+
+        return Stdio::inherit();
     }
 }
 
@@ -44,7 +61,7 @@ impl Cd<'_> {
 }
 
 impl Execute for Cd<'_> {
-    fn execute(&self) {
+    fn execute(&self, _: Stdio, _: bool) -> Stdio {
         let new_dir = self.args[1];
 
         let root = Path::new(new_dir);
@@ -52,5 +69,33 @@ impl Execute for Cd<'_> {
         if let Err(e) = env::set_current_dir(&root) {
             eprintln!("{}", e);
         }
+
+        return Stdio::inherit();
+    }
+}
+
+pub struct Pipe<'a> {
+    command1: Box<dyn Execute + 'a>,
+    command2: Box<dyn Execute + 'a>,
+}
+
+impl Pipe<'_> {
+    pub fn new<'a>(command1: Box<dyn Execute + 'a>, command2: Box<dyn Execute + 'a>) -> Pipe<'a> {
+        Pipe { command1, command2 }
+    }
+}
+
+impl Execute for Pipe<'_> {
+    fn execute(&self, stdin: Stdio, out: bool) -> Stdio {
+        self.command2
+            .execute(self.command1.execute(stdin, false), out)
+    }
+}
+
+pub struct False {}
+
+impl Execute for False {
+    fn execute(&self, _: Stdio, _: bool) -> Stdio {
+        return Stdio::inherit();
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,54 @@
-use commands::{Cd, CommandSystem, Execute};
+use commands::{Cd, CommandSystem, Execute, False, Pipe};
+use std::process::Stdio;
 
 mod commands;
 
 pub fn execute(line: &str) {
     let args: Vec<&str> = line.trim().split_whitespace().collect();
 
-    parser(&args).execute();
+    parser(&args).execute(Stdio::inherit(), true);
+}
+
+fn priority_command(arg: &str) -> u16 {
+    match arg {
+        "|" => 2,
+        ">" => 2,
+        ">>" => 2,
+        "<" => 2,
+        _ => 0,
+    }
 }
 
 fn parser<'a>(args: &'a [&str]) -> Box<dyn Execute + 'a> {
-    match args[0] {
+    let mut ind = 0;
+    let mut priority = 1;
+
+    for i in 0..args.len() {
+        let aux = priority_command(args[i]);
+
+        if aux > priority {
+            priority = aux;
+            ind = i;
+        }
+    }
+
+    match args[ind] {
+        "|" => {
+            let c1 = if ind != 0 {
+                parser(&args[0..ind])
+            } else {
+                eprintln!("Incorrect Pipe");
+                Box::new(False {})
+            };
+            let c2 = if ind != args.len() - 1 {
+                parser(&args[ind + 1..])
+            } else {
+                eprintln!("Incorrect Pipe");
+                Box::new(False {})
+            };
+
+            Box::new(Pipe::new(c1, c2))
+        }
         "cd" => Box::new(Cd::new(args)),
         _ => Box::new(CommandSystem::new(args[0], &args[1..])),
     }

5e2f481c1b6ad7ef3327d893c7f5bf4a4952ee78
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-04-30 21:51:22 -0400

    fix format

--- src/format_line.rs
+++ src/format_line.rs
@@ -1,38 +1,15 @@
 use crate::error;
 
 pub fn format_line(line: String) -> Vec<String> {
-    let pat = [
-        ("&", true),
-        ("|", true),
-        (";", false),
-        ("<", false),
-        (">", true),
-        ("(", false),
-        (")", false),
-    ];
-
-    let new_line = stop_line(line);
-    let (mut new_line, v) = encode_c(new_line);
-
-    for i in pat {
-        new_line = format_pat(&new_line, i.0, i.1);
-    }
-
-    new_line = encode_command(new_line);
-    new_line = eliminate_spaces(new_line);
+    let new_line = format_encode(line);
+    let new_line = encode_command_set(new_line);
 
     let args: Vec<&str> = new_line.split_whitespace().collect();
 
-    decode_c(args, v)
+    decode(args)
 }
 
-fn stop_line(line: String) -> String {
-    let mut args = line.split("#");
-
-    return String::from(args.next().unwrap());
-}
-
-fn encode_command(line: String) -> String {
+fn encode_command_set(line: String) -> String {
     let mut new_line = String::new();
 
     let mut o = true;
@@ -54,58 +31,15 @@ fn encode_command(line: String) -> String {
     return new_line;
 }
 
-fn eliminate_spaces(line: String) -> String {
-    let mut new_line = String::new();
-    let args: Vec<&str> = line.split_whitespace().collect();
-
-    for i in 0..args.len() - 1 {
-        if args[i] == "" {
-            continue;
-        }
-
-        new_line.push_str(format!("{} ", args[i]).as_str());
-    }
-
-    new_line.push_str(args[args.len() - 1]);
-
-    new_line
-}
-
-fn encode_c(line: String) -> (String, Vec<String>) {
-    let mut new_line = String::new();
-    let mut v = Vec::new();
-
-    let args: Vec<&str> = line.split("\"").collect();
-
-    if args.len() % 2 == 0 {
-        eprintln!("{} incorrect format line", error());
-
-        return (String::from("false"), v);
-    }
-
-    for i in 0..args.len() {
-        if i % 2 == 0 {
-            new_line.push_str(args[i]);
-        } else {
-            v.push(args[i].to_string());
-            new_line.push_str("\"")
-        }
-    }
-
-    (new_line, v)
-}
-
-fn decode_c(args: Vec<&str>, v: Vec<String>) -> Vec<String> {
+fn decode(args: Vec<&str>) -> Vec<String> {
     let mut new_args: Vec<String> = Vec::new();
 
-    let mut j = 0;
     for arg in args {
         let mut aux = String::new();
 
         for i in arg.chars() {
-            if i == '"' {
-                aux.push_str(v[j].as_str());
-                j += 1;
+            if i == '#' {
+                aux.push(' ');
             } else {
                 aux.push(i);
             }
@@ -117,28 +51,101 @@ fn decode_c(args: Vec<&str>, v: Vec<String>) -> Vec<String> {
     new_args
 }
 
-fn format_pat(line: &String, pat: &str, par: bool) -> String {
+fn format_encode(line: String) -> String {
+    let line: Vec<char> = line.chars().collect();
     let mut new_line = String::new();
 
-    let args: Vec<&str> = line.split(pat).collect();
+    let mut c = false;
+    let mut cont = false;
 
-    if args.len() == 1 {
-        new_line.push_str(line.trim());
-        return new_line;
-    }
-
-    new_line.push_str(format!("{} {}", args[0].trim(), pat).as_str());
-
-    for i in 1..args.len() - 1 {
-        if args[i] == "" && par {
-            new_line.push_str(pat);
+    for i in 0..line.len() {
+        if cont {
+            cont = false;
             continue;
-        };
+        }
 
-        new_line.push_str(format!(" {} {}", args[i].trim(), pat).as_str());
+        if line[i] == '#' {
+            break;
+        }
+
+        if line[i] == '"' {
+            c = !c;
+            continue;
+        }
+
+        if c {
+            if line[i] == ' ' {
+                new_line.push('#');
+            } else {
+                new_line.push(line[i])
+            };
+            continue;
+        }
+
+        if line[i] == '\\' {
+            if i != line.len() - 1 && line[i + 1] == ' ' {
+                new_line.push('#');
+                cont = true;
+            }
+            continue;
+        }
+
+        if line[i] == ' ' {
+            if i != 0 {
+                if line[i - 1] == ' ' {
+                    continue;
+                }
+            } else {
+                continue;
+            }
+        }
+
+        let stop = format_pat(&line, i, &mut new_line);
+
+        if stop {
+            continue;
+        }
+
+        new_line.push(line[i]);
     }
 
-    new_line.push_str(format!(" {}", args[args.len() - 1].trim()).as_str());
+    if c {
+        eprintln!("{} incorrect format line", error());
 
-    return new_line;
+        return String::from("false");
+    }
+
+    new_line
+}
+
+fn format_pat(line: &[char], ind: usize, new_line: &mut String) -> bool {
+    let pat = [
+        ('&', true),
+        ('|', true),
+        (';', false),
+        ('<', false),
+        ('>', true),
+        ('(', false),
+        (')', false),
+    ];
+
+    for (x, y) in pat {
+        if line[ind] == x {
+            if ind != 0 {
+                if line[ind - 1] != ' ' && line[ind - 1] != x {
+                    new_line.push(' ');
+                }
+            }
+            new_line.push(line[ind]);
+            if ind != line.len() - 1 {
+                if line[ind + 1] != ' ' && (!y || line[ind + 1] != x) {
+                    new_line.push(' ');
+                }
+            }
+
+            return true;
+        }
+    }
+
+    false
 }

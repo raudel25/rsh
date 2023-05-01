0947ffded0e2f925eba814f18fe8ffa5800fea79
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-05-01 01:01:50 -0400

    special format

--- src/format_line.rs
+++ src/format_line.rs
@@ -1,49 +1,31 @@
 use crate::error;
 
 pub fn format_line(line: String) -> Vec<String> {
-    let new_line = format_encode(line);
-    let new_line = encode_command_set(new_line);
+    let (new_line, special_spaces) = format_encode(line);
 
     let args: Vec<&str> = new_line.split_whitespace().collect();
 
-    decode(args)
+    decode(args, special_spaces)
 }
 
-fn encode_command_set(line: String) -> String {
-    let mut new_line = String::new();
-
-    let mut o = true;
-
-    for i in line.chars() {
-        if i == '`' {
-            if o {
-                new_line.push_str("( ");
-            } else {
-                new_line.push_str(" )");
-            };
-        } else {
-            new_line.push(i);
-        }
-
-        o = i == ' ';
-    }
-
-    return new_line;
-}
-
-fn decode(args: Vec<&str>) -> Vec<String> {
+fn decode(args: Vec<&str>, special_spaces: Vec<usize>) -> Vec<String> {
     let mut new_args: Vec<String> = Vec::new();
+    let mut x = 0;
+    let mut j = 0;
 
     for arg in args {
         let mut aux = String::new();
 
         for i in arg.chars() {
-            if i == '#' {
+            if special_spaces.len() != x && j == special_spaces[x] {
                 aux.push(' ');
+                x += 1;
             } else {
                 aux.push(i);
             }
+            j += 1;
         }
+        j += 1;
 
         new_args.push(aux);
     }
@@ -51,9 +33,10 @@ fn decode(args: Vec<&str>) -> Vec<String> {
     new_args
 }
 
-fn format_encode(line: String) -> String {
+fn format_encode(line: String) -> (String, Vec<usize>) {
     let line: Vec<char> = line.chars().collect();
     let mut new_line = String::new();
+    let mut special_spaces: Vec<usize> = Vec::new();
 
     let mut c = false;
     let mut cont = false;
@@ -64,10 +47,6 @@ fn format_encode(line: String) -> String {
             continue;
         }
 
-        if line[i] == '#' {
-            break;
-        }
-
         if line[i] == '"' {
             c = !c;
             continue;
@@ -75,32 +54,21 @@ fn format_encode(line: String) -> String {
 
         if c {
             if line[i] == ' ' {
-                new_line.push('#');
+                format_special_spaces(&mut new_line, &mut special_spaces);
             } else {
                 new_line.push(line[i])
             };
             continue;
         }
 
-        if line[i] == '\\' {
-            if i != line.len() - 1 && line[i + 1] == ' ' {
-                new_line.push('#');
-                cont = true;
-            }
-            continue;
+        if line[i] == '#' {
+            break;
         }
 
-        if line[i] == ' ' {
-            if i != 0 {
-                if line[i - 1] == ' ' {
-                    continue;
-                }
-            } else {
-                continue;
-            }
-        }
-
-        let stop = format_pat(&line, i, &mut new_line);
+        let mut stop = format_special(&line, i, &mut new_line, &mut special_spaces, &mut cont);
+        stop = stop || format_spaces(&line, i);
+        stop = stop || format_set(&line, i, &mut new_line);
+        stop = stop || format_pat(&line, i, &mut new_line);
 
         if stop {
             continue;
@@ -112,10 +80,10 @@ fn format_encode(line: String) -> String {
     if c {
         eprintln!("{} incorrect format line", error());
 
-        return String::from("false");
+        return (String::from("false"), special_spaces);
     }
 
-    new_line
+    (new_line, special_spaces)
 }
 
 fn format_pat(line: &[char], ind: usize, new_line: &mut String) -> bool {
@@ -148,4 +116,66 @@ fn format_pat(line: &[char], ind: usize, new_line: &mut String) -> bool {
     }
 
     false
+}
+
+fn format_spaces(line: &[char], ind: usize) -> bool {
+    if line[ind] == ' ' {
+        if ind != 0 {
+            if line[ind - 1] == ' ' {
+                return true;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn format_set(line: &[char], ind: usize, new_line: &mut String) -> bool {
+    if line[ind] == '`' {
+        if ind == 0 || line[ind - 1] == ' ' {
+            new_line.push_str("( ");
+        } else {
+            new_line.push_str(" )");
+        };
+
+        return true;
+    }
+
+    false
+}
+
+fn format_special(
+    line: &[char],
+    ind: usize,
+    new_line: &mut String,
+    special_spaces: &mut Vec<usize>,
+    cont: &mut bool,
+) -> bool {
+    let special = ['\\', '"', '`', ' ', '#'];
+
+    if line[ind] == '\\' {
+        if ind != line.len() - 1 {
+            for x in special {
+                if x == line[ind + 1] {
+                    if x != ' ' {
+                        new_line.push(x);
+                    } else {
+                        format_special_spaces(new_line, special_spaces);
+                    }
+                    *cont = true;
+                    break;
+                }
+            }
+        }
+        return true;
+    }
+
+    false
+}
+
+fn format_special_spaces(new_line: &mut String, special_spaces: &mut Vec<usize>) {
+    special_spaces.push(new_line.len());
+    new_line.push('#');
 }
